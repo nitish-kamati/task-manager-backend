f53047014caace1782b047f8c1be319ef689f37f
nitish-kamati <ntsh02kamati@gmail.com>
2026-05-03 22:34:33 +0530

    fixed cors issue

--- task-manager/src/main/java/com/nitish/task_manager/config/CorsConfig.java
+++ task-manager/src/main/java/com/nitish/task_manager/config/CorsConfig.java
@@ -12,7 +12,10 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",   // local frontend
+                "https://task-manager-frontend-drab-one.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
